d26bb7e41089435f2baf2b6bd1ad3ff2b29cae8c
AlexMarPapas <122617586+AlexMarPapas@users.noreply.github.com>
2023-03-21 18:07:09 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const path = require('path');
 const port = 3000;
 
 const app = express();
@@ -13,11 +14,14 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
   });
 
+  app.get('/resume', (req, res) => {
+    const resumePath = path.join(__dirname, 'public', 'resume', 'Resume_CV');
+    res.download(resumePath);
+  });
 
 app.get("*", function (req, res) {
     res.redirect("https://www.gmail.com/")
   });
-
 
 app.listen(port, function () {
     console.log("The server is running on the port " + port);
